82a6825a7b315d2206aca85fdce63677f45dc52e
Kenneth.So <kennethso@lycos.com>
2014-10-30 16:33:41 +0800

    Fixes bluetooth connection

--- RemoteControl/src/WorkerRunnable.java
+++ RemoteControl/src/WorkerRunnable.java
@@ -1,9 +1,11 @@
 import java.awt.AWTException;
+import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class WorkerRunnable implements Runnable {
@@ -17,6 +19,11 @@ public class WorkerRunnable implements Runnable {
 	public void run() {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            out.write(">ACCEPTED");
+            out.flush();
+
+            //DataInputStream is = new DataInputStream(client.getInputStream());
 
             try {
             	SystemInputExecutor key = SystemInputExecutor.getInstance();
@@ -26,12 +33,27 @@ public class WorkerRunnable implements Runnable {
                 	System.out.println(message);
 
                 	if (">NEXT".equalsIgnoreCase(message)) {
-                		key.left();
-                	} else if (">PREV".equalsIgnoreCase(message)) {
                 		key.right();
+                	} else if (">PREV".equalsIgnoreCase(message)) {
+                		key.left();
                 	}
 
-                	done = message.equals(".quit");
+//                	String message = null;
+//                	byte[] bytes = new byte[1024];
+//                    int r;
+//
+//                    while ((r = is.read(bytes)) > 0) {
+//                        message = new String(bytes, 0, r);
+//                        System.out.println(message);
+//
+//                        if (">NEXT".equalsIgnoreCase(message)) {
+//                        	key.right();
+//                    	} else if (">PREV".equalsIgnoreCase(message)) {
+//                    		key.left();
+//                    	}
+//
+//                    }
+                	done = ".quit".equals(message);
                 }
             } catch (AWTException e) {
             	e.printStackTrace();
